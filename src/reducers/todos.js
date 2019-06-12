ea4317b35d78b217758c82d3c9154245b9cd1901
andreastkdf <andreaskostakis@gmail.com>
2019-06-12 18:57:44 +0100

    use imported 'TOGGLE_TODO' and 'RECEIVE_DATA'

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -8,13 +8,13 @@ export default function todos(state = [], action) {
       return state.concat([action.todo])
     case REMOVE_TODO:
       return state.filter(todo => todo.id !== action.id)
-    case "TOGGLE_TODO":
+    case TOGGLE_TODO:
       return state.map(todo =>
         todo.id !== action.id
           ? todo
           : Object.assign({}, todo, { complete: !todo.complete })
       )
-    case "RECEIVE_DATA":
+    case RECEIVE_DATA:
       return action.todos
     default:
       return state
